d8aed1a44fc006cb5631ebfc6e3c4810bc7e183a
Alike73 <alimzhanisla@gmail.com>
2024-03-17 23:47:34 -0400

    U

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,10 +1,21 @@
+import SearchWeatherInput from "../components/SearchWeatherInput";
+import WeatherSVG from "../components/WeatherSVG";
 
 
 const Main = () => {
 
     return (
-        <div>
-            <h1>Main</h1>
+        <div className="main">
+            <div className="container-fluid col-xl-10 col-xxl-8 px-4 py-5">
+                <div className="col text-center text-lg-start">
+                    <h1 className="display-4 fw-bold lh-1 text-body-emphasis mb-5">
+                        Sky Seeker
+                    </h1>
+                    <SearchWeatherInput />
+                    <h2 className="display-5 fw-bold lh-1 text-body-emphasis text-center mb-3">City name, country</h2>
+                    <WeatherSVG />
+                </div>
+            </div>
         </div>
     )
 };
